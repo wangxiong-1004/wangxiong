01aa7a90c638c193e398147a3d012cbb0482c2a9
wangxiong <xiong.wang02@bqjr.cn>
2019-09-04 10:38:35 +0800

    util

---
+++ wx_vue_webpack/src/assets/util.js
@@ -0,0 +1,20 @@
+export default {
+    /**
+     * 将数组分割成多元数组
+     * @method splitArrToMultivariateArr
+     * @param { number } num 每个数组长度
+     * @param { array } arr 原数组
+     * @example
+     * arr = [1, 2, 3, 4, 5, 6, 7]
+     * splitArrToMultivariateArr(2, arr) => [[1, 2], [3, 4], [5, 6], [7]]
+     */
+    splitArrToMultivariateArr(num, arr) {
+        const MultivariateArr = [];
+
+        for (let i = 0; i < arr.length;) {
+            MultivariateArr.push(arr.slice(i, i += num));
+        }
+
+        return MultivariateArr;
+    }
+};
