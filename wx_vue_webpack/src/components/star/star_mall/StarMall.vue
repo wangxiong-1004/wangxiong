9735be188388fc970e36216bb82f5c8d197d8383
wangxiong <xiong.wang02@bqjr.cn>
2019-09-05 18:20:11 +0800

    eslint

--- wx_vue_webpack/src/components/star/star_mall/StarMall.vue
+++ wx_vue_webpack/src/components/star/star_mall/StarMall.vue
@@ -6,52 +6,55 @@
 </template>
 
 <script>
-/* eslint-disable */
-    const STAR_LENGTH = 5;
-    const CLS_ON = 'on';
-    const CLS_HALF = 'half';
-    const CLS_OFF = 'off';
+const STAR_LENGTH = 5;
+const CLS_ON = 'on';
+const CLS_HALF = 'half';
+const CLS_OFF = 'off';
 
-    export default {
-        props: {
-            size: {
-                type: Number,
-                default: 28
-            },
-            score: {
-                type: Number,
-                default: 5
-            }
+export default {
+    props: {
+        size: {
+            type: Number,
+            default: 28
         },
-        computed: {
-            starType() {
-                return 'star-' + this.size;
-            },
-            itemClasses() {
-                let result = [];
-                let score = Math.floor(this.score * 2) / 2;
-                let hasDecimal = score % 1 !== 0;
-                let integer = Math.floor(score);
-                for (let i = 0; i < integer; i++) {
-                    result.push(CLS_ON);
-                }
-                if (hasDecimal) {
-                    result.push(CLS_HALF);
-                }
-                while (result.length < STAR_LENGTH) {
-                    result.push(CLS_OFF);
-                }
-                return result;
-            }
-        },
-
-        methods: {
-            starMarkChange() {
-                console.log(222)
-                this.$emit('starMarkChangeName', 111);
-            }
+        score: {
+            type: Number,
+            default: 5
         }
-    };
+    },
+    computed: {
+        starType() {
+            return `star-${this.size}`;
+        },
+
+        itemClasses() {
+            const result = [];
+            const score = Math.floor(this.score * 2) / 2;
+            const hasDecimal = score % 1 !== 0;
+            const integer = Math.floor(score);
+
+            for (let i = 0; i < integer; i++) {
+                result.push(CLS_ON);
+            }
+
+            if (hasDecimal) {
+                result.push(CLS_HALF);
+            }
+
+            while (result.length < STAR_LENGTH) {
+                result.push(CLS_OFF);
+            }
+
+            return result;
+        }
+    },
+
+    methods: {
+        starMarkChange() {
+            this.$emit('starMarkChangeName', 111);
+        }
+    }
+};
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
