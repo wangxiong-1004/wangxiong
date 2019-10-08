9c65b28f662b9758cfdd7a2a93873205a9acdad0
wangxiong <xiong.wang02@bqjr.cn>
2019-10-08 18:29:34 +0800

    formatter code

--- wx_vue_webpack/src/axios/index.js
+++ wx_vue_webpack/src/axios/index.js
@@ -4,9 +4,9 @@ import util from './util';
 
 console.log(config);
 const instance = axios.create({
-    baseURL: config.BASE_URL,
-    timeout: config.TIMEOUT,
-    headers: {}
+  baseURL: config.BASE_URL,
+  timeout: config.TIMEOUT,
+  headers: {}
 });
 
 // post请求头
@@ -14,58 +14,58 @@ instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlenco
 
 // 请求拦截器
 instance.interceptors.request.use((config) => {
-    console.log(config);
-    if (config.showLoading) {
-        util.showFullScreenLoading();
-        console.log('show loading');
-    }
-    return config;
+  console.log(config);
+  if (config.showLoading) {
+    util.showFullScreenLoading();
+    console.log('show loading');
+  }
+  return config;
 }, (error) => {
-    console.log(error);
-    return Promise.error(error);
+  console.log(error);
+  return Promise.error(error);
 });
 
 // 响应拦截器
 instance.interceptors.response.use((response) => {
-    console.log(response);
-    if (response.config.showLoading) {
-        util.tryHideFullScreenLoading();
-        console.log('hide loading');
-    }
+  console.log(response);
+  if (response.config.showLoading) {
+    util.tryHideFullScreenLoading();
+    console.log('hide loading');
+  }
 
-    if (response.status === 200) {
-        return Promise.resolve(response);
-    }
+  if (response.status === 200) {
+    return Promise.resolve(response);
+  }
 
-    return Promise.reject(response);
+  return Promise.reject(response);
 }, error => Promise.reject(error.response));
 
 export function get({ url, params }) {
-    return new Promise((resolve, reject) => {
-        instance.get(url, {
-            params: {
-                data: params
-            },
-            showLoading: true
-        })
-            .then((res) => {
-                resolve(res);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    instance.get(url, {
+      params: {
+        data: params
+      },
+      showLoading: true
+    })
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 export function post({ url, params }) {
-    return new Promise((resolve, reject) => {
-        instance
-            .post(url, params)
-            .then((res) => {
-                resolve(res);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    instance
+      .post(url, params)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
