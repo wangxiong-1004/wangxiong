80ec04914c2e6aa7a01cd082cced791ef243c46b
wangxiong <xiong.wang02@bqjr.cn>
2019-10-12 15:02:35 +0800

    js

---
+++ ts/01.js
@@ -0,0 +1,15 @@
+var isDone = true;
+var num = 10;
+var names = 'wng';
+var str = "this name is " + names;
+var list = [1, 2, 3];
+var list2 = [1, 2, 3];
+var x;
+x = ['string', 10];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Blue;
---
+++ ts/01.ts
@@ -0,0 +1,16 @@
+let isDone: boolean = true;
+let num: number = 10;
+let names: string = 'wng';
+let str: string = `this name is ${names}`;
+let list: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+let x: [string, number];
+x = ['string', 10];
+
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let c: Color = Color.Blue;
